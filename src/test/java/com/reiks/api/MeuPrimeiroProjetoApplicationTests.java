59bb62c8893c7f41c9a56f4663f051a106b8c0d9
Claudia Seara <claudiaseara@gmail.com>
2019-11-30 14:07:39 -0300

    Versão inicial do projeto

---
+++ src/test/java/com/reiks/api/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reiks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
